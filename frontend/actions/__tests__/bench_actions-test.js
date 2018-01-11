08e4fbee7f1debc27fb97f6a19b110060306f4ad
reid <reidsherman@gmail.com>
2018-01-11 14:53:49 -0800

    Test synchronous action creators

--- frontend/actions/__tests__/bench_actions-test.js
+++ frontend/actions/__tests__/bench_actions-test.js
@@ -8,30 +8,36 @@ import { testBenches } from '../../testUtil/bench_helper';
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-// describe('actions', () => {
-//   test('receiveBenches should create an action to receive benches', () => {
-//     // refer to https://redux.js.org/docs/recipes/WritingTests.html 
-//   })
-// });
+describe('actions', () => {
+  test('receiveBenches should create an action to receive benches', () => {
+    // refer to https://redux.js.org/docs/recipes/WritingTests.html 
+    expect(actions.receiveBenches(testBenches)).toEqual(
+      {
+        type: actions.RECEIVE_BENCHES,
+        benches: testBenches
+      }
+    );
+  });
+});
 
-// describe('async actions', () => {
+describe('async actions', () => {
 
-  // test('fetchBenches creates RECEIVE_BENCHES after fetching benches', () => {
-  //   // REFER TO REDUX TESTS DOCS
-  //   // Set up expectedActions:
-  //   // Your code here
+  test('fetchBenches creates RECEIVE_BENCHES after fetching benches', () => {
+    // REFER TO REDUX TESTS DOCS
+    // Set up expectedActions:
+    // Your code here
 
-  //   ApiUtil.fetchBenches = jest.fn(() => {
-  //     return Promise.resolve(testBenches);
-  //   });
+    ApiUtil.fetchBenches = jest.fn(() => {
+      return Promise.resolve(testBenches);
+    });
 
-  //   const store = mockStore({ benches: {} });
+    const store = mockStore({ benches: {} });
 
-  //   return store.dispatch(actions.fetchBenches()).then(() => {
-  //     // Your code here
-  //   });
-  // });
-// });
+    return store.dispatch(actions.fetchBenches()).then(() => {
+      // Your code here
+    });
+  });
+});
 
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
